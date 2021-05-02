3b303414e63e0367a84d4e3e75e77223f320a161
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2021-05-02 16:17:38 -0700

    Add structs section 2 notes

---
+++ structs/src/main.rs
@@ -0,0 +1,35 @@
+// With tuples
+
+// fn main() {
+//     let rect1 = (30, 50);
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(rect1)
+//     );
+// }
+
+// fn area(dimensions: (u32, u32)) -> u32 {
+//     dimensions.0 * dimensions.1
+// }
+
+// With structs
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("rect1 is {:#?}", rect1);
+    println!("The area of the rectangle is {} square pixels", area(&rect1));
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
